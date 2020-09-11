25af7b055af300a39f3f3daf18b2956e904a4ba1
justinsadakhom <justin.sadakhom@gmail.com>
2020-09-11 13:58:01 -0400

    Add test suite for skill.js

---
+++ test/cards/skill.test.js
@@ -0,0 +1,43 @@
+import {Skill, SkillNames} from '../../src/modules/classes/cards/skill'
+import { Player } from '../../src/modules/classes/player'
+import { RoleCard, Roles } from '../../src/modules/classes/cards/role_card'
+import { CharacterCard, Characters } from '../../src/modules/classes/cards/character_card'
+
+test("Constructor with invalid name parameter throws TypeError", () => {
+    let skill
+    expect(() => {
+        skill = new Skill('Name', 'Ace', 'Spades')
+    }).toThrow(TypeError('Invalid skill name!'))
+})
+
+test("Skill.isPlayable() when player doesn't already have the skill", () => {
+    let players = []
+    let player = new Player(new RoleCard(Roles.PROTAGONIST), new CharacterCard(Characters.KAEDE))
+    let card = new Skill('Trust', 'Ace', 'Spades')
+
+    expect(card.isPlayable(player, players)).toBe(true)
+})
+
+test("Skill.isPlayable() when player already has the skill", () => {
+    let players = []
+    let player = new Player(new RoleCard(Roles.PROTAGONIST), new CharacterCard(Characters.KAEDE))
+
+    let card = new Skill('Trust', 'Ace', 'Spades')
+    let dupe = new Skill('Trust', 'King', 'Spades')
+    player.hand.push(dupe)
+    player.addSkill(0)
+
+    expect(card.isPlayable(player, players)).toBe(false)
+})
+
+test("Skill.play() correctly adds skill to player", () => {
+    let players = []
+    let player = new Player(new RoleCard(Roles.PROTAGONIST), new CharacterCard(Characters.KAEDE))
+
+    let card = new Skill('Trust', 'Ace', 'Spades')
+    player.hand.push(card)
+    player.addSkill(0)
+
+    expect(player.hand.length).toBe(1)
+    expect(player.hasSkill(SkillNames.TRUST))
+})
